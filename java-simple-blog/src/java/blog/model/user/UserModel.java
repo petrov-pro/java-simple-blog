a14a53ae2075939482f7213bf9e112dd490c8ca1
Andrey Petrov <petroff@favoritbet.com>
2015-09-30 19:15:51 +0300

    fix loger

--- java-simple-blog/src/java/blog/model/user/UserModel.java
+++ java-simple-blog/src/java/blog/model/user/UserModel.java
@@ -11,7 +11,7 @@ import blog.system.exception.PersistException;
 import blog.system.loader.Load;
 import blog.system.model.Model;
 import blog.system.tools.BindParams;
-import javax.servlet.ServletException;
+import blog.system.tools.Logger;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
@@ -23,115 +23,116 @@ import javax.validation.ValidatorFactory;
 //test
 public class UserModel extends Model {
 
-    private String errorMessage = "";
+	private String errorMessage = "";
 
-    private User user;
+	private User user;
 
-    public UserModel() {
-        user = new User();
-    }
+	public UserModel() {
+		user = new User();
+	}
 
-    public String getErrorMessage() {
-        return errorMessage;
-    }
+	public String getErrorMessage() {
+		return errorMessage;
+	}
 
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
-    }
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    @Override
-    public Object getData() {
-        return this;
-    }
+	@Override
+	public Object getData() {
+		return this;
+	}
 
-    @Override
-    public String getView() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public String getView() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
 
-    @Override
-    public Object getNavigator() {
-        return null;
-    }
+	@Override
+	public Object getNavigator() {
+		return null;
+	}
 
-    public boolean create() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-        BindParams.bind(user);
-        if (User.validate(user, validator)) {
-            UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
-            Long result;
-            try {
-                result = ui.insertAdv(user);
-            } catch (PersistException p) {
-                result = null;
-            }
-            if (result == null) {
-                errorMessage = Load.bundle.getString("user_cant_insert");
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            errorMessage = User.getErrorMessage();
-            return false;
-        }
-    }
+	public boolean create() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		BindParams.bind(user);
+		if (User.validate(user, validator)) {
+			UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
+			Long result;
+			try {
+				result = ui.insertAdv(user);
+			} catch (PersistException p) {
+				Logger.write(p.toString());
+				result = null;
+			}
+			if (result == null) {
+				errorMessage = Load.bundle.getString("user_cant_insert");
+				return false;
+			} else {
+				return true;
+			}
+		} else {
+			errorMessage = User.getErrorMessage();
+			return false;
+		}
+	}
 
-    public boolean update(int user_id) {
-        this.getUserInfo(user_id);
-        user.setEmail(Load.request.getParameter("email"));
-        user.setPassword(Load.request.getParameter("password"));
-        UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
-        Boolean result;
-        try {
-            if (user.getPassword().isEmpty()) {
-                ui.setStrUpdateWithoutPass();
-            } else {
-                ui.setStrUpdateWithPass();
-            }
-            result = ui.update(user);
-        } catch (PersistException p) {
-            result = false;
-        }
-        if (result == false) {
-            errorMessage = Load.bundle.getString("user_cant_update");
-            return false;
-        } else {
-            return true;
-        }
-    }
+	public boolean update(int user_id) {
+		this.getUserInfo(user_id);
+		user.setEmail(Load.request.getParameter("email"));
+		user.setPassword(Load.request.getParameter("password"));
+		UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
+		Boolean result;
+		try {
+			if (user.getPassword().isEmpty()) {
+				ui.setStrUpdateWithoutPass();
+			} else {
+				ui.setStrUpdateWithPass();
+			}
+			result = ui.update(user);
+		} catch (PersistException p) {
+			result = false;
+		}
+		if (result == false) {
+			errorMessage = Load.bundle.getString("user_cant_update");
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-    public boolean checkUnique(String name) {
-        UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
-        User u = ui.findByUserName(name);
-        if (u == null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public boolean checkUnique(String name) {
+		UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
+		User u = ui.findByUserName(name);
+		if (u == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public boolean getUserInfo(int user_id) {
-        UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
-        try {
-            user = ui.findByPk(user_id);
-        } catch (PersistException p) {
-            throw new RuntimeException();
-        }
-        if (user == null) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+	public boolean getUserInfo(int user_id) {
+		UserImpl ui = (UserImpl) DaoFactory.getDao("UserImpl");
+		try {
+			user = ui.findByPk(user_id);
+		} catch (PersistException p) {
+			throw new RuntimeException();
+		}
+		if (user == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
 }
